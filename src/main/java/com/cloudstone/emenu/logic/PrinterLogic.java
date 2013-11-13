7ed0622cdee86cfd4035191d96fc65d82696d3b3
czwcarelife@gmail.com <czwcarelife@gmail.com>
2013-11-14 01:42:40 +0800

    REFACTOR: printLogic code style

--- src/main/java/com/cloudstone/emenu/logic/PrinterLogic.java
+++ src/main/java/com/cloudstone/emenu/logic/PrinterLogic.java
@@ -2,6 +2,7 @@
  * @(#)PrinterLogic.java, Aug 13, 2013. 
  * 
  */
+
 package com.cloudstone.emenu.logic;
 
 import java.util.ArrayList;
@@ -39,81 +40,211 @@ import com.cloudstone.emenu.wrap.RecordWraper;
 
 /**
  * @author xuhongfeng
- *
  */
 @Component
 public class PrinterLogic extends BaseLogic {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(PrinterLogic.class);
-    
+
     @Autowired
     private DishWraper dishWraper;
+
     @Autowired
     private RecordWraper recordWraper;
+
     @Autowired
     private MenuLogic menuLogic;
+
     @Autowired
     private RecordLogic recordLogic;
+
     @Autowired
     private VelocityRender velocityRender;
+
     @Autowired
     private IPrintComponentDb printComponentDb;
+
     @Autowired
     private IPrintTemplateDb printTemplateDb;
+
     @Autowired
     private IPrinterConfigDb printerConfigDb;
-    
+
     public String[] listPrinters() {
         return PrinterUtils.listPrinters();
     }
+
     public void printBill(EmenuContext context, Bill bill, User user) throws Exception {
         String[] printers = listPrinters();
-        for (String printer:printers) {
+        for (String printer : printers) {
             PrinterConfig config = getPrinterConfig(context, printer);
             if (config != null && config.isWhenBill()) {
-                for (int templateId:config.getBillTemplateIds()) {
+                for (int templateId : config.getBillTemplateIds()) {
                     LOG.info("print templateId :" + templateId);
                     printBill(context, bill, user, printer, templateId);
                 }
             }
         }
     }
-    
-    public void printBill(EmenuContext context, Bill bill, User user, String printer, int templateId) throws Exception {
+
+    public void printBill(EmenuContext context, Bill bill, User user, String printer, int templateId)
+            throws Exception {
         PrintTemplate template = getTemplate(context, templateId);
         if (template != null) {
             String templateString = getTemplateString(context, template, 0, template.getCutType());
-            List<DishRecord> cancelRecords = recordLogic.listCancelDishRecords(context, bill.getOrderId());
-            List<DishRecord> addRecords = recordLogic.listAddDishRecords(context, bill.getOrderId());
+            List<DishRecord> cancelRecords = recordLogic.listCancelDishRecords(context,
+                    bill.getOrderId());
+            List<DishRecord> addRecords = recordLogic
+                    .listAddDishRecords(context, bill.getOrderId());
             List<RecordVO> cancelRecordVOs = recordWraper.wrapRecord(context, cancelRecords);
             List<RecordVO> addRecordVOs = recordWraper.wrapRecord(context, addRecords);
 
-            
-            if (template.getCutType() == Const.CutType.PER_DISH && bill.getOrder().getDishes().size()>0) {
-                for (OrderDishVO dish:bill.getOrder().getDishes()) {
+            if (template.getCutType() == Const.CutType.PER_DISH
+                    && bill.getOrder().getDishes().size() > 0) {
+                for (OrderDishVO dish : bill.getOrder().getDishes()) {
                     List<OrderDishVO> dishes = new LinkedList<OrderDishVO>();
                     dishes.add(dish);
-                    List<DishGroup> dishGroups = dishWraper.wrapDishGroup(context, dishes, template.getChapterIds(), true);
+                    List<DishGroup> dishGroups = dishWraper.wrapDishGroup(context, dishes,
+                            template.getChapterIds(), true);
                     if (!CollectionUtils.isEmpty(dishGroups)) {
-                        String content = velocityRender.renderBill(bill, user, dishGroups, cancelRecordVOs, addRecordVOs, templateString);
+                        String content = velocityRender.renderBill(bill, user, dishGroups,
+                                cancelRecordVOs, addRecordVOs, templateString);
                         PrinterUtils.print(printer, content, template.getFontSize());
                     }
                 }
             } else {
-                List<DishGroup> dishGroups = dishWraper.wrapDishGroup(context, bill.getOrder().getDishes(), template.getChapterIds(), true);
+                List<DishGroup> dishGroups = dishWraper.wrapDishGroup(context, bill.getOrder()
+                        .getDishes(), template.getChapterIds(), true);
                 if (!CollectionUtils.isEmpty(dishGroups)) {
-                    String content = velocityRender.renderBill(bill, user, dishGroups, cancelRecordVOs, addRecordVOs, templateString);
+                    String content = velocityRender.renderBill(bill, user, dishGroups,
+                            cancelRecordVOs, addRecordVOs, templateString);
                     PrinterUtils.print(printer, content, template.getFontSize());
                 }
             }
         }
     }
-    
-    private String getTemplateString(EmenuContext context, PrintTemplate template, int type, int cutType) {
+
+    public void printCancelOrder(EmenuContext context, OrderVO order, User user) throws Exception {
+        String[] printers = listPrinters();
+        for (String printer : printers) {
+            PrinterConfig config = getPrinterConfig(context, printer);
+            if (config != null && config.isWhenCancel()) {
+                for (int templateId : config.getCancelTemplateIds()) {
+                    LOG.info("print templateId :" + templateId);
+                    printOrder(context, order, user, printer, templateId);
+                }
+            }
+        }
+    }
+
+    public void printAddOrder(EmenuContext context, OrderVO order, User user) throws Exception {
+        String[] printers = listPrinters();
+        for (String printer : printers) {
+            PrinterConfig config = getPrinterConfig(context, printer);
+            if (config != null && config.isWhenAdd()) {
+                for (int templateId : config.getAddTemplateIds()) {
+                    LOG.info("print templateId :" + templateId);
+                    printOrder(context, order, user, printer, templateId);
+                }
+            }
+        }
+    }
+
+    public void printOrder(EmenuContext context, OrderVO order, User user) throws Exception {
+        String[] printers = listPrinters();
+        for (String printer : printers) {
+            PrinterConfig config = getPrinterConfig(context, printer);
+            if (config != null && config.isWhenOrdered()) {
+                for (int templateId : config.getOrderedTemplateIds()) {
+                    LOG.info("print templateId :" + templateId);
+                    printOrder(context, order, user, printer, templateId);
+                }
+            }
+        }
+    }
+
+    public void printOrder(EmenuContext context, OrderVO order, User user, String printer,
+            int templateId) throws Exception {
+        PrintTemplate template = getTemplate(context, templateId);
+        if (template != null) {
+            String templateString = getTemplateString(context, template, 1, template.getCutType());
+            if (template.getCutType() == Const.CutType.PER_DISH && order.getDishes().size() > 0) {
+                for (OrderDishVO dish : order.getDishes()) {
+                    List<OrderDishVO> dishes = new LinkedList<OrderDishVO>();
+                    dishes.add(dish);
+                    List<DishGroup> dishGroups = dishWraper.wrapDishGroup(context, dishes,
+                            template.getChapterIds(), false);
+                    if (!CollectionUtils.isEmpty(dishGroups)) {
+                        String content = velocityRender.renderOrder(order, user, dishWraper
+                                .wrapDishGroup(context, dishes, template.getChapterIds(), false),
+                                templateString);
+                        PrinterUtils.print(printer, content, template.getFontSize());
+                    }
+                }
+            } else {
+                List<DishGroup> dishGroups = dishWraper.wrapDishGroup(context, order.getDishes(),
+                        template.getChapterIds(), false);
+                if (!CollectionUtils.isEmpty(dishGroups)) {
+                    String content = velocityRender.renderOrder(order, user, dishGroups,
+                            templateString);
+                    PrinterUtils.print(printer, content, template.getFontSize());
+                }
+            }
+        }
+    }
+
+    public PrintTemplate getTemplate(EmenuContext context, int id) {
+        PrintTemplate template = printTemplateDb.get(context, id);
+        checkChapterIds(context, template);
+        return template;
+    }
+
+    public List<PrintComponent> listComponents(EmenuContext context) {
+        List<PrintComponent> list = printComponentDb.listAll(context);
+        DataUtils.filterDeleted(list);
+        return list;
+    }
+
+    public PrintComponent addComponent(EmenuContext context, PrintComponent data) {
+        printComponentDb.add(context, data);
+        return printComponentDb.get(context, data.getId());
+    }
+
+    public PrintComponent updateComponent(EmenuContext context, PrintComponent data) {
+        PrintComponent old = printComponentDb.get(context, data.getId());
+        if (old == null || old.isDeleted()) {
+            throw new NotFoundException("该页眉页脚不存在");
+        }
+        printComponentDb.update(context, data);
+        return printComponentDb.get(context, data.getId());
+    }
+
+    public PrintComponent getComponent(EmenuContext context, int id) {
+        return printComponentDb.get(context, id);
+    }
+
+    public void deleteComponent(EmenuContext context, int id) {
+        PrintComponent old = printComponentDb.get(context, id);
+        if (old == null || old.isDeleted()) {
+            throw new NotFoundException("该页眉页脚不存在");
+        }
+        printComponentDb.delete(context, id);
+        printTemplateDb.removeComponent(context, id);
+    }
+
+    public List<PrintTemplate> listTemplate(EmenuContext context) {
+        List<PrintTemplate> list = printTemplateDb.listAll(context);
+        DataUtils.filterDeleted(list);
+        checkChapterIds(context, list);
+        return list;
+    }
+
+    private String getTemplateString(EmenuContext context, PrintTemplate template, int type,
+            int cutType) {
         StringBuilder sb = new StringBuilder();
         int headerId = template.getHeaderId();
         int footerId = template.getFooterId();
-        
+
         if (headerId != 0) {
             PrintComponent header = getComponent(context, headerId);
             if (header != null) {
@@ -141,130 +272,20 @@ public class PrinterLogic extends BaseLogic {
         return sb.toString();
     }
 
-    public void printCancelOrder(EmenuContext context, OrderVO order, User user) throws Exception {
-        String[] printers = listPrinters();
-        for (String printer:printers) {
-            PrinterConfig config = getPrinterConfig(context, printer);
-            if (config != null && config.isWhenCancel()) {
-                for (int templateId:config.getCancelTemplateIds()) {
-                    LOG.info("print templateId :" + templateId);
-                    printOrder(context, order, user, printer, templateId);
-                }
-            }
-        }
-    }
-
-    public void printAddOrder(EmenuContext context, OrderVO order, User user) throws Exception {
-        String[] printers = listPrinters();
-        for (String printer:printers) {
-            PrinterConfig config = getPrinterConfig(context, printer);
-            if (config != null && config.isWhenAdd()) {
-                for (int templateId:config.getAddTemplateIds()) {
-                    LOG.info("print templateId :" + templateId);
-                    printOrder(context, order, user, printer, templateId);
-                }
-            }
-        }
-    }
-
-    public void printOrder(EmenuContext context, OrderVO order, User user) throws Exception {
-        String[] printers = listPrinters();
-        for (String printer:printers) {
-            PrinterConfig config = getPrinterConfig(context, printer);
-            if (config != null && config.isWhenOrdered()) {
-                for (int templateId:config.getOrderedTemplateIds()) {
-                    LOG.info("print templateId :" + templateId);
-                    printOrder(context, order, user, printer, templateId);
-                }
-            }
-        }
-    }
-    
-    public void printOrder(EmenuContext context, OrderVO order, User user, String printer, int templateId) throws Exception {
-        PrintTemplate template = getTemplate(context, templateId);
-        if (template != null) {
-            String templateString = getTemplateString(context, template, 1, template.getCutType());
-            if (template.getCutType() == Const.CutType.PER_DISH && order.getDishes().size()>0) {
-                for (OrderDishVO dish:order.getDishes()) {
-                    List<OrderDishVO> dishes = new LinkedList<OrderDishVO>();
-                    dishes.add(dish);
-                    List<DishGroup> dishGroups = dishWraper.wrapDishGroup(context, dishes, template.getChapterIds(), false);
-                    if (!CollectionUtils.isEmpty(dishGroups)) {
-                        String content = velocityRender.renderOrder(order, user,
-                                dishWraper.wrapDishGroup(context, dishes, template.getChapterIds(), false), templateString);
-                        PrinterUtils.print(printer, content, template.getFontSize());
-                    }
-                }
-            } else {
-                List<DishGroup> dishGroups = dishWraper.wrapDishGroup(context, order.getDishes(), template.getChapterIds(), false);
-                if (!CollectionUtils.isEmpty(dishGroups)) {
-                    String content = velocityRender.renderOrder(order, user, dishGroups, templateString);
-                    PrinterUtils.print(printer, content, template.getFontSize());
-                }
-            }
-        }
-    }
-
-    public PrintTemplate getTemplate(EmenuContext context, int id) {
-        PrintTemplate template = printTemplateDb.get(context, id);
-        checkChapterIds(context, template);
-        return template;
-    }
-    
-    public List<PrintComponent> listComponents(EmenuContext context) {
-        List<PrintComponent> list = printComponentDb.listAll(context);
-        DataUtils.filterDeleted(list);
-        return list;
-    }
-    
-    public PrintComponent addComponent(EmenuContext context, PrintComponent data) {
-        printComponentDb.add(context, data);
-        return printComponentDb.get(context, data.getId());
-    }
-    
-    public PrintComponent updateComponent(EmenuContext context, PrintComponent data) {
-        PrintComponent old = printComponentDb.get(context, data.getId());
-        if (old==null || old.isDeleted()) {
-            throw new NotFoundException("该页眉页脚不存在");
-        }
-        printComponentDb.update(context, data);
-        return printComponentDb.get(context, data.getId());
-    }
-    
-    public PrintComponent getComponent(EmenuContext context, int id) {
-        return printComponentDb.get(context, id);
-    }
-    
-    public void deleteComponent(EmenuContext context, int id) {
-        PrintComponent old = printComponentDb.get(context, id);
-        if (old==null || old.isDeleted()) {
-            throw new NotFoundException("该页眉页脚不存在");
-        }
-        printComponentDb.delete(context, id);
-        printTemplateDb.removeComponent(context, id);
-    }
-    
-    public List<PrintTemplate> listTemplate(EmenuContext context) {
-        List<PrintTemplate> list = printTemplateDb.listAll(context);
-        DataUtils.filterDeleted(list);
-        checkChapterIds(context, list);
-        return list;
-    }
-    
     private void checkChapterIds(EmenuContext context, List<PrintTemplate> templates) {
-        for (PrintTemplate template:templates) {
+        for (PrintTemplate template : templates) {
             checkChapterIds(context, template);
         }
     }
-    
+
     private void checkChapterIds(EmenuContext context, PrintTemplate template) {
-        //check chapterId exists
+        // check chapterId exists
         if (!CollectionUtils.isEmpty(template.getChapterIds())) {
             int[] chapterIds = menuLogic.getChapterIds(context);
             Arrays.sort(chapterIds);
             List<Integer> newIds = new LinkedList<Integer>();
             boolean needUpdate = false;
-            for (int oldId:template.getChapterIds()) {
+            for (int oldId : template.getChapterIds()) {
                 if (Arrays.binarySearch(chapterIds, oldId) < 0) {
                     needUpdate = true;
                 } else {
@@ -277,36 +298,36 @@ public class PrinterLogic extends BaseLogic {
             }
         }
     }
-    
+
     public PrintTemplate addTemplate(EmenuContext context, PrintTemplate template) {
         printTemplateDb.add(context, template);
         return printTemplateDb.get(context, template.getId());
     }
-    
+
     public PrintTemplate updateTemplate(EmenuContext context, PrintTemplate template) {
         int headerId = template.getHeaderId();
         if (headerId != 0) {
             PrintComponent header = getComponent(context, headerId);
-            if (header==null || header.isDeleted()) {
+            if (header == null || header.isDeleted()) {
                 throw new NotFoundException("该页眉不存在");
             }
         }
         int footerId = template.getFooterId();
         if (footerId != 0) {
             PrintComponent footer = getComponent(context, footerId);
-            if (footer==null || footer.isDeleted()) {
+            if (footer == null || footer.isDeleted()) {
                 throw new NotFoundException("该页脚不存在");
             }
         }
         printTemplateDb.update(context, template);
         return printTemplateDb.get(context, template.getId());
     }
-    
+
     public void deleteTemplate(EmenuContext context, int id) {
         printTemplateDb.delete(context, id);
         printerConfigDb.removeTemplate(context, id);
     }
-    
+
     public PrinterConfig getPrinterConfig(EmenuContext context, String name) {
         PrinterConfig config = printerConfigDb.getConfig(context, name);
         if (config == null) {
@@ -316,26 +337,26 @@ public class PrinterLogic extends BaseLogic {
         }
         return config;
     }
-    
+
     public PrinterConfig getPrinterConfig(EmenuContext context, int id) {
         String[] printers = listPrinters();
-        for (String p:printers) {
+        for (String p : printers) {
             if (p.hashCode() == id) {
                 return getPrinterConfig(context, p);
             }
         }
         return null;
     }
-    
+
     public PrinterConfig updatePrinterConfig(EmenuContext context, PrinterConfig config) {
         printerConfigDb.update(context, config);
         return printerConfigDb.getConfig(context, config.getName());
     }
-    
+
     public List<PrinterConfig> listPrinterConfig(EmenuContext context) {
         String[] printers = listPrinters();
         List<PrinterConfig> configs = new ArrayList<PrinterConfig>();
-        for (String name:printers) {
+        for (String name : printers) {
             configs.add(getPrinterConfig(context, name));
         }
         return configs;
