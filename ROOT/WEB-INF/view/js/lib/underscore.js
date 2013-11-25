339d29fcfd88e4133c42fcdf9b68afcaa5286eb3
Chenzhuwei <carelife@Chenzhuweis-MacBook-Air.local>
2013-11-25 15:28:20 +0800

    Recover: master branch

--- ROOT/WEB-INF/view/js/lib/underscore.js
+++
@@ -1,9 +0,0 @@
-/*jslint browser: true, devel: true, indent: 4, nomen:true, vars: true */
-/*global define */
-
-define(function (require, exports, module) {
-    "use strict";
-
-    return window._.noConflict();
-});
-
